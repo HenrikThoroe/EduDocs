e08214e51653c1fde495b76c480d04615ac28a43
Henrik Thoroe <henrik@thoroee.com>
2020-08-11 21:40:57 +0200

    Add 'File' type

---
+++ src/shared/File.ts
@@ -0,0 +1,33 @@
+import path from "path"
+import fs from "fs/promises"
+
+export default class File {
+
+    private readonly url: string
+
+    readonly path: string
+
+    readonly name: string
+
+    readonly type: string
+
+    constructor(url: string) {
+        if (!path.isAbsolute(url)) {
+            throw new Error(`Path ${url} must be absolute.`)
+        }
+
+        this.url = url
+        this.type = path.extname(url)
+        this.name = path.basename(url)
+        this.path = path.dirname(url)
+    }
+
+    get absolutePath(): string {
+        return this.url
+    }
+
+    async read(): Promise<string> {
+        return await fs.readFile(this.absolutePath, { encoding: "utf8" })
+    }
+
+}
