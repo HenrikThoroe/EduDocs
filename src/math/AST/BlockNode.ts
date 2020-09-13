c2b1f6327c60b00fb84fabd88a7bd89a69e21504
Henrik <henrik@thoroee.com>
2020-09-13 13:55:03 +0200

    Add lines() method

--- src/math/AST/BlockNode.ts
+++ src/math/AST/BlockNode.ts
@@ -12,4 +12,23 @@ export default class BlockNode implements ASTNode {
         return this.children.map(child => child.toString()).join(seperator)
     }
 
+    lines(): BlockNode[] {
+        const lines: BlockNode[] = []
+        let last = new BlockNode([])
+        const add = () => lines.push(new BlockNode([...last.children]))
+
+        for (const node of this.children) {
+            last.children.push(node)
+
+            if (node.type === "symbol" && node.toString().startsWith("\\\\")) {
+                add()
+                last.children = []
+            }
+        }
+
+        add()
+
+        return lines
+    }
+
 }
